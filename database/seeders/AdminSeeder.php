2607b45dad1fa56397c5c5b412aafa8c1c7596c0
Obalowu Sodiq <osodiq@gmail.com>
2021-02-06 07:18:58 +0100

    Model and migration set up

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name'=>'Admin',
+            'email'=>'test@test.com',
+            'password'=>'password'
+        ]);
+    }
+}
