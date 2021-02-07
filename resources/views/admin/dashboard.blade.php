1844dd185d95c40a1afc887a3d4228143babcc9f
skilfulsidiq <osodiq@gmail.com>
2021-02-07 14:28:36 +0100

    errad

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,19 +15,20 @@
                 </div><!-- media-body -->
               </div><!-- media -->
             </div><!-- card -->
+            </a>
             </div>
           </div><!-- col-3 -->
           <div class="col-sm-6 col-md-6 mg-t-10 mg-sm-t-0">
                <a href="{{route('admin.employee')}}">
-            <div class="card card-status">
-              <div class="media">
-                <i class="icon ion-ios-people-outline tx-teal"></i>
-                <div class="media-body">
-                  <h1>{{$total_users}}</h1>
-                  <p>Total Employee</p>
-                </div><!-- media-body -->
-              </div><!-- media -->
-            </div><!-- card -->
+                <div class="card card-status">
+                  <div class="media">
+                    <i class="icon ion-ios-people-outline tx-teal"></i>
+                    <div class="media-body">
+                      <h1>{{$total_users}}</h1>
+                      <p>Total Employee</p>
+                    </div><!-- media-body -->
+                  </div><!-- media -->
+                </div><!-- card -->
                </a>
           </div><!-- col-3 -->
          <!-- col-3 -->
